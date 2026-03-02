d38178f35323d84eda5b38f7fd2e397f8090d493
paulo-dutraa83 <paulo.dutraa83@gmail.com>
2026-03-02 12:55:09 -0300

    Criação de componentes, rotas, formularios, httpclient, signal e cadastro de usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AutenticarUsuario } from './pages/autenticar-usuario/autenticar-usuario';
+import { CadastrarUsuario } from './pages/cadastrar-usuario/cadastrar-usuario';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : "pages/autenticar-usuario", //Rota de navegação
+        component: AutenticarUsuario //classe do componente
+    },
+    {
+        path : "pages/cadastrar-usuario", //Rota de navegação
+        component: CadastrarUsuario
+    },
+    {
+        path: "", //Rota de navegação para a pagina inicial
+        redirectTo: "pages/autenticar-usuario", //Redirecionamento'
+        pathMatch: "full" //correspondencia completa da rota
+    }
+];
