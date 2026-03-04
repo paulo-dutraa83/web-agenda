b126bb0090dc7490d94e681a1217e1cceda76c84
paulo-dutraa83 <paulo.dutraa83@gmail.com>
2026-03-04 12:56:08 -0300

    Validações vindo do back e msgs de erro

--- src/app/pages/autenticar-usuario/autenticar-usuario.ts
+++ src/app/pages/autenticar-usuario/autenticar-usuario.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-autenticar-usuario',
   imports: [
-    RouterLink
+    RouterLink,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './autenticar-usuario.html',
   styleUrl: './autenticar-usuario.css',
 })
-export class AutenticarUsuario {}
+export class AutenticarUsuario {
+
+  //Injeção de dependencia
+  private http = inject(HttpClient);
+  
+  //mensagens
+  mensagemErro = signal<string>('');  
+
+  //Criando a estrutura do formulario
+  formulario = new FormGroup({
+    email : new FormControl('', [Validators.required]),
+    senha : new FormControl('', [Validators.required]),
+  });
+
+  //Função para realizar a autenticação do usuário
+  autenticar() {
+    
+    //Fazendo uma requisição HTTP POST para o endpoint de autenticação
+    this.http.post('http://localhost:8082/api/v1/usuario/autenticar', this.formulario.value)
+      .subscribe({
+        next: (data) => { //Capturando a resposta de sucesso
+          console.log(data); //Exibindo os dados no console
+          alert('Sucesso! Voce  será redirecionado para a agenda"'); //Provisorio.
+        },
+        error: (e) => { //Capturando a resposta de erro
+          this.mensagemErro.set(e.error); //Exibindo a mensagem de erro
+        }
+      });
+
+  }  
+
+}
