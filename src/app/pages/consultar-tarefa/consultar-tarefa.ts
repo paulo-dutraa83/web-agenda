21703366140d3b147b2cce2a1f863c289baf7175
paulo-dutraa83 <paulo.dutraa83@gmail.com>
2026-03-23 12:55:36 -0300

    Consulta de tarefas por data e exclusão de datas

--- src/app/pages/consultar-tarefa/consultar-tarefa.ts
+++ src/app/pages/consultar-tarefa/consultar-tarefa.ts
@@ -3,13 +3,16 @@ import { Sidebar } from '../../layout/sidebar/sidebar';
 import { Navbar } from '../../layout/navbar/navbar';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-consultar-tarefa',
   imports: [
     Navbar,
     Sidebar,
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './consultar-tarefa.html',
   styleUrl: './consultar-tarefa.css',
@@ -22,11 +25,62 @@ export class ConsultarTarefa {
   //HTTP Client para fazer a integração com a API
   private http = inject(HttpClient);
 
+  //Formulario para filtrar as datas da pesquisa
+  formulario = new FormGroup({
+    dataMin: new FormControl('', [Validators.required]),
+    dataMax: new FormControl('', [Validators.required]),
+  });
+
   //Função exacutada ao abrir a pagina
   ngOnInit() {
-    this.http.get('http://localhost:8083/api/v1/tarefas')
+    //Pegando a data atual
+    const hoje = new Date();
+
+    //Primeiro dia do mês
+    const primeiroDia = new Date(hoje.getFullYear(), hoje.getMonth(), 1);
+    //Ultimo dia do mês
+    const ultimoDia = new Date(hoje.getFullYear(), hoje.getMonth() + 1, 0);
+
+    //Formatando as datas para o formato yyyy-MM-dd
+    const formatarData = (data: Date) => {
+      return data.toISOString().split('T')[0];
+    }
+    //Preenchendo o formularo com as datas e do primeiro e ultimo dia do mes
+    this.formulario.patchValue({
+      dataMin: formatarData(primeiroDia),
+      dataMax: formatarData(ultimoDia)
+    });
+
+    //Realizando a consulta
+    this.consultar();
+  }
+
+  //Função para executar a consulta das tarefas
+  consultar() {
+    //Capturando as data do formulario
+    const dataMin = this.formulario.value.dataMin;
+    const dataMax = this.formulario.value.dataMax;
+
+    //Executando a consulta na API de tarefas
+    this.http.get('http://localhost:8083/api/v1/tarefas/' + dataMin + "/" + dataMax)
       .subscribe((data) => {
         this.tarefas.set(data as any[]);
       });
   }
+
+  //Função para excluir a tarefa
+  excluir(id: string) {
+    if (confirm('Deseja realmente excluir a tarefa selecionada?')) {
+      this.http.delete('http://localhost:8083/api/v1/tarefas/' + id)
+        .subscribe({
+          next: (data: any) => {
+            this.consultar();
+          },
+          error: (e) => {
+            console.log(e.error);
+          }
+        });
+    }
+  }
+
 }
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -2,17 +2,34 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { Sidebar } from '../../layout/sidebar/sidebar';
 import { Navbar } from '../../layout/navbar/navbar';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Chart, ChartModule } from 'angular-highcharts';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     CommonModule,
     Sidebar,
-    Navbar
+    Navbar,
+    FormsModule,
+    ReactiveFormsModule,
+    ChartModule
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+
+  graficoDonut = signal<Chart>(new Chart());
+  graficoBarras = signal<Chart>(new Chart());
+
+  formulario = new FormGroup({
+    dataMin: new FormControl('', [Validators.required]),
+    dataMax: new FormControl('', [Validators.required]),
+  });
+
+  consultar() {
+    
+  }
 
 }
